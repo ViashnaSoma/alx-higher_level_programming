71b164ee08b8782cc30b8a1d1fdb7f3ee520d1c0
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-07-10 16:03:20 +0200

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,31 @@
+#include "lists.h"
 
+/**
+ * check_cycle - detects loops
+ * @list: linked list head
+ *
+ * Description - detects loops
+ * Return: 1 for cycled, 0 for nothing found
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (!list)
+	{
+		return (0);
+	}
+	slow = list;
+	fast = list->next;
+	while (fast && slow && fast->next)
+	{
+		if (slow == fast)
+		{
+			return (1);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
