fa1cfc6c58b50a103ae932dd2f23d342e4ae7a82
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-08-01 16:47:14 +0200

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a Square."""
+
+
+class Square:
+    """Define a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): Size of a new square.
+        """
+        self.__size = size
