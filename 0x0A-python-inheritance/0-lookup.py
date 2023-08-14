4f25bf9e29215618d96c884acf0df1383e1eca60
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-08-14 16:33:30 +0200

    Create 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+This module returns all attributes of an object
+
+"""
+
+
+def lookup(obj):
+    """
+    This function returns all attributes of the object
+    """
+
+    return dir(obj)
